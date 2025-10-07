ff4c1019c60decac5d4b5a7ede83b4b5ac00f2c7
Sofie De Weer <sofie.deweer@esat.kuleuven.be>
2025-10-07 10:26:53 +0200

    fix: sigmoid function SCA

--- ising/solvers/SCA.py
+++ ising/solvers/SCA.py
@@ -86,15 +86,15 @@ class SCA(SolverBase):
                 hs = np.matmul(J, state) + model.h
 
                 Prob = self.get_prob(hs, state, q, T)
-                rand = np.random.rand(N)
+                rand = np.random.uniform(0, 1)
 
-                flipped_states = [y for y in range(N) if Prob[y] < rand[y]]
+                flipped_states = [y for y in range(N) if Prob[y] < rand]
 
                 tau[flipped_states] = -state[flipped_states]
                 state = np.copy(tau)
 
                 if log.filename is not None:
-                    energy = model.evaluate(state)
+                    energy = model.evaluate(state.astype(np.float32))
                     log.log(energy=energy, state=state)
 
                 T = self.change_hyperparam(T, cooling_rate)
@@ -127,13 +127,6 @@ class SCA(SolverBase):
         Returns:
             probability (np.ndarray): probability of accepting the change of all nodes.
         """
-        values = np.multiply(hs, sample) + q
-        probs = np.zeros_like(values)
-        for i,val in enumerate(values):
-            if val > 2*T:
-                probs[i] = 1
-            elif val < -2*T:
-                probs[i] = 0
-            else:
-                probs[i] = val/(4*T) + 0.5
+        values = (np.multiply(hs, sample) + q)/(2*T)
+        probs = 1/(1+np.exp(-values))
         return probs
